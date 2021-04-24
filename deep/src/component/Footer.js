7ae83a751e50d8113c18b534b689cd49b65051af
Brillianttyagi <deepanshubhai84@gmail.com>
2021-04-24 17:49:30 +0530

    updated website

--- deep/src/component/Footer.js
+++ deep/src/component/Footer.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react'
 import { Container,Row,Col } from 'react-bootstrap'
 import './css/Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-import { faFacebook,faGooglePlus,faTwitter,faYoutube,faLinkedin } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope, faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub,faCodiepie,faTwitter,faLinkedin, faInstagram } from "@fortawesome/free-brands-svg-icons"
 
 class Foot extends Component {
     render() { 
@@ -14,11 +14,11 @@ class Foot extends Component {
                         <div class="col-5 offset-1 col-sm-2">
                             <h5>Links</h5>
                             <ul class="list-unstyled">
-                                <li><a id="links" href="#">Home</a></li>
-                                <li><a id="links" href="./aboutus.html">Projects</a></li>
+                                <li><a id="links" href="/">Home</a></li>
+                                <li><a id="links" href="./project">Projects</a></li>
                                 <li><a id="links" href="#">Resume</a></li>
-                                <li><a id="links" href="./contactus.html">Contact</a></li>
-                                <li><a id="links" href="./contactus.html">About</a></li>
+                                <li><a id="links" href="./contact">Contact</a></li>
+                                <li><a id="links" href="./about">About</a></li>
                             </ul>
                         </div>
                     </Col>
@@ -35,11 +35,11 @@ class Foot extends Component {
                     </Col>
                     <Col>
                         <div>
-                            <a id="social_" class="btn btn-social-icon btn-google-plus" href="http://google.com/+"><FontAwesomeIcon icon={faGooglePlus} size="2x" /></a>
-                            <a id="social_" class="btn btn-social-icon btn-facebook" href="http://www.facebook.com/profile.php?id="><FontAwesomeIcon icon={faFacebook} size="2x" /></a>
-                            <a id="social_" class="btn btn-social-icon btn-linkedin" href="http://www.linkedin.com/in/"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-                            <a id="social_" class="btn btn-social-icon btn-twitter" href="http://twitter.com/"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
-                            <a id="social_" class="btn btn-social-icon btn-youtube" href="http://youtube.com/"><FontAwesomeIcon icon={faYoutube} size="2x" /></a>
+                            <a id="social_" class="btn btn-social-icon btn-google-plus" href="https://github.com/Brillianttyagi"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+                            <a id="social_" class="btn btn-social-icon btn-facebook" href="https://twitter.com/imaprogramr"><FontAwesomeIcon icon={faTwitter} size="2x" /></a>
+                            <a id="social_" class="btn btn-social-icon btn-linkedin" href="https://www.instagram.com/ttyagi_boy84/"><FontAwesomeIcon icon={faInstagram} size="2x" /></a>
+                            <a id="social_" class="btn btn-social-icon btn-twitter" href="https://www.codechef.com/users/yesimaprogramr"><FontAwesomeIcon icon={faCodiepie} size="2x" /></a>
+                            <a id="social_" class="btn btn-social-icon btn-youtube" href="https://www.linkedin.com/in/deepanshu-tyagi-355855198/"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
                             <a id="social_" class="btn btn-social-icon" href="mailto:"><FontAwesomeIcon icon={faEnvelope} size="2x" /></a>
                         </div>
                     </Col>
